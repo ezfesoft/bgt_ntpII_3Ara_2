731442ee8541e3c6731387a70fc03e1484c918e7
slymn_LAB2 <ezfesoftware@gmail.com>
2024-12-03 11:17:02 +0300

    Proje dosyası ekle.

---
+++ bgt_ntpII_3Ara_2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace bgt_ntpII_3Ara_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Met1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(136, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Met2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(260, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 53);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Met3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(384, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 53);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Met4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(384, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(118, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 113);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ bgt_ntpII_3Ara_2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bgt_ntpII_3Ara_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Met1();
+        }
+
+        public void Met1()
+        {
+            DateTime dt= DateTime.Now; 
+            MessageBox.Show(dt.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string b = textBox1.Text;
+            Met2(b);
+        }
+        public void Met2(string a)
+        {
+            MessageBox.Show("Hoşgeldiniz "+a);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int x, sonuc;
+            x = 15;
+            sonuc = Met3() + x;
+            MessageBox.Show(sonuc.ToString());
+        }
+        public int Met3()
+        {
+            return 5;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int z = Convert.ToInt32(textBox2.Text);
+            if (Met4(z))
+            {
+                MessageBox.Show("Doğru");
+            }
+            else
+            {
+                MessageBox.Show("Yanlış");
+            }
+        }
+
+        public bool Met4(int y)
+        {
+            return (y * y) > (25);
+            
+        }
+
+    }
+}
